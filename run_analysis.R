de26faddf798de91c703b21db8938c904bbc56a2
Martha Eugenia <MarthaEugenia@users.noreply.github.com>
2014-07-27 18:54:00 -0500

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,46 @@
+
+# ------- 1. Merges the training and the test sets to create one data set.
+
+# Reading the data for training set
+features <- read.table("./UCI HAR Dataset/features.txt")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt",col.names=c("subject_id"))
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt",col.names=features[,2])
+Y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt",col.names=c("activity_id"))
+
+# Merging the data for training set
+training_set <- cbind(subject_train,Y_train, X_train)
+
+# Reading the data for test set
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt",col.names=c("subject_id"))
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt",col.names=features[,2])
+Y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt",col.names=c("activity_id"))
+
+# Merging the data for test set
+test_set <- cbind(subject_test,Y_test, X_test)
+
+# Merging the training and test sets
+tt_dataset <- rbind(training_set,test_set)
+
+# ------- 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+
+# Looks for mean and standard deviation from features
+features_mean_std <- features[grepl("mean\\(\\)", features$V2) | grepl("std\\(\\)", features$V2), ]
+# Keeps the first two columns (subject, Y) and the previously selected features
+dataset_mean_std <- tt_dataset[,c(c(1,2),features_mean_std$V1+2)]
+
+# ------- 3. Uses descriptive activity names to name the activities in the data set
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt",col.names=c("activity_id","activity_labels"))
+dataset_mean_std <- merge(dataset_mean_std,activity_labels)
+
+# ------- 4. Appropriately labels the data set with descriptive variable names. 
+
+# Change some variables names, changing  "." to "_"
+dataset_labels <- colnames(dataset_mean_std)
+dataset_labels <- gsub("\\.std","_Std", dataset_labels)
+dataset_labels <- gsub("\\.mean","_Mean", dataset_labels)
+dataset_labels <- gsub("\\...","_", dataset_labels)
+dataset_labels <- gsub("\\..","", dataset_labels)
+colnames(dataset_mean_std) <- dataset_labels
+
+
+write.csv(file="final_data.txt", x=dataset_mean_std)
